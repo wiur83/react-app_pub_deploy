5f5a69b0e039c59cd861dd0e12f6e9e6a03a1b32
witold urbas <wiur83@gmail.com>
2020-03-11 22:31:07 +0100

    Updates

---
+++ precache-manifest.1efd16658094e1896b7f4d1a1fb7d9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ad541f528e0319b94fa7fbea08ba79",
+    "url": "/react-app_pub_deploy/index.html"
+  },
+  {
+    "revision": "6e98e4073ae3c69abd56",
+    "url": "/react-app_pub_deploy/static/css/main.9e2b74d1.chunk.css"
+  },
+  {
+    "revision": "689ca857e48acc74b000",
+    "url": "/react-app_pub_deploy/static/js/2.a44205a8.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/react-app_pub_deploy/static/js/2.a44205a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e98e4073ae3c69abd56",
+    "url": "/react-app_pub_deploy/static/js/main.e4da149f.chunk.js"
+  },
+  {
+    "revision": "0b1dbb45d497e7db0dd8",
+    "url": "/react-app_pub_deploy/static/js/runtime-main.cbfdb9aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app_pub_deploy/precache-manifest.f128e051fc3afdea3bb35705c7ffa5be.js"
+  "/react-app_pub_deploy/precache-manifest.1efd16658094e1896b7f4d1a1fb7d9f9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-app_pub_deploy/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app_pub_deploy/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
