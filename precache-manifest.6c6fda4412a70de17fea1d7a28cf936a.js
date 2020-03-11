d506169e7161f53f5196b097bf5ffbb8e5416683
witold urbas <wiur83@gmail.com>
2020-03-11 21:40:15 +0100

    Updates

---
+++ precache-manifest.6c6fda4412a70de17fea1d7a28cf936a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6935b106e00429ea11b3",
+    "url": "/react-app_pub_deploy/static/js/runtime~main.6935b106.js"
+  },
+  {
+    "revision": "b53da84aa30f13d2d8d7",
+    "url": "/react-app_pub_deploy/static/js/main.b53da84a.chunk.js"
+  },
+  {
+    "revision": "8477a699f3fb2ae60745",
+    "url": "/react-app_pub_deploy/static/js/1.8477a699.chunk.js"
+  },
+  {
+    "revision": "b53da84aa30f13d2d8d7",
+    "url": "/react-app_pub_deploy/static/css/main.02d25d75.chunk.css"
+  },
+  {
+    "revision": "81b3184fd249a3d72bed88010e349653",
+    "url": "/react-app_pub_deploy/index.html"
+  }
+];
